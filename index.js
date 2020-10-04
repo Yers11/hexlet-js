8e80e3299a57036150204c5d7ac87255fe12ff07
Yersaiyn <yersdeli@gmail.com>
2020-10-04 21:12:10 +0600

    some commit

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import _ from "lodash";
 console.log("Hello, hexlet");
 console.log("it is a js file");
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"])); console.log("asd");
